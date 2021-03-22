0d2f0f36315dca69c91116dbd873850118a40ff2
shlee39 <39shlee@gmail.com>
2021-03-22 15:17:04 +0900

    1

--- src/main/java/com/sh/s2/robot/Arm_Right.java
+++ src/main/java/com/sh/s2/robot/Arm_Right.java
@@ -2,10 +2,8 @@ package com.sh.s2.robot;
 
 import org.springframework.stereotype.Component;
 
-@Component("ar")
+
 public class Arm_Right extends Arm{
 	
-	public Arm_Right() {
-		this.setName("Right Arm");
-	}
+	
 }
--- src/test/java/com/sh/s2/robot/RobotTest.java
+++ src/test/java/com/sh/s2/robot/RobotTest.java
@@ -11,16 +11,17 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*-context.xml"})
 public class RobotTest {
-
+	
 	@Autowired
 	private Robot robot;
+	//private Arm_Left arm_Left;
+	
 	
 	@Test
 	public void test() {
-		//left_arm의 name
-		System.out.println(robot.getArm_Left().getName());
-		//right_arm의 name
-		System.out.println(robot.getArm_Right().getName());
+		
+		assertNotNull(robot.getArm_Right()); //성공하면 null이 아니라는 것이다 = true
+		
 	}
-
+	
 }
